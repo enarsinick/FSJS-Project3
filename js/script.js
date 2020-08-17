8073c23fdd103ce5a7c2a9e081ca398df4121d89
Nick Cave <nxdmedia@googlemail.com>
2020-08-17 20:21:58 +0100

    Added feature to avoid confliciting activites be selected at the same time

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+
+// Global variables
 const nameField = document.getElementById('name');
 const jobField = document.getElementById('other-title');
 const colourSelect = document.getElementById('color'); 
 const designSelect = document.getElementById('design');
+const checkboxes = document.querySelectorAll('.activities input');
+let totalCostHeader = document.createElement('h3');
+totalCost = 0;
 
-// FUNCTIONS
+// Function that shows certain option within the t-shirt colour 
+// option drop down menu, depending on the data passed to it
 const makeColourOptionsHidden = (options, num, length) => {
     for (let i = num; i < length; i++) {
         options[i].hidden = true;
@@ -25,77 +31,61 @@ window.addEventListener("load", function(){
     placeholderOption.disabled = true;
     placeholderOption.innerHTML = 'Please select a T-shirt theme';
     colourSelect.appendChild(placeholderOption);
+
+    // Appending total activities cost to the page on load
+    totalCostHeader.innerHTML = `Total: $${totalCost}`;
+    document.querySelector('.activities').append(totalCostHeader);
 });
 
+// Listens out for changes on the t-shirt info drop down
 designSelect.addEventListener('change', (event) => {
     let selected = event.target.value;
 
+    // Check certain value of selected and hide/show certain options in colour drop down
     if(selected === 'js puns') {
         makeColourOptionsHidden(colourSelect, 0, colourSelect.length);
         for(let i = 0; i <= 2; i++) {
             colourSelect[i].hidden = false;
         }
-
     } else if (selected === 'heart js') {
         makeColourOptionsHidden(colourSelect, 0, colourSelect.length);
         for(let i = 3; i <= 5; i++) {
             colourSelect[i].hidden = false;
         }
-        
     } else {
         console.log('Please select a design');
     }
 });
 
+// Listen out for changes on the activities checkboxes
+document.querySelector('.activities').addEventListener('change', (e) => {
+    // Store the clicked checkbox, it's cost and day/time value
+    let clicked = e.target;
+    let clickedCost = clicked.getAttribute('data-cost');
+    clickedCost = parseInt(clickedCost);
+    let clickedTime = clicked.getAttribute('data-day-and-time');
 
-// // 1) Follow the steps below to hide the "Select a shirt" option tag so users won't be able to select it or see it in the drop down menu
-//   // 1a) Target the "Select a shirt" `option` element with a querySelector, like so — ('option[value="Select a shirt"]') 
-//   // 1b) Use dot notation to set the `hidden` property to true on the "Select a shirt" `option` element 
-//   document.querySelector('option[value="Select a shirt"]').hidden = true;
+    // check if current clicked checkbox has been checked and do some math
+    if (clicked.checked) {
+        totalCost += clickedCost;
+    } else {
+        totalCost -= clickedCost;
+    }
+    // Update total cost header on page
+    totalCostHeader.innerHTML = `Total: $${totalCost}`;
 
-// // 2) Follow the steps below to practice manually selecting which option will show in the "Shirt Selection" field
-//   const shirtSelectElement = document.querySelector('#shirt-select-element');
-//   const shirtOptionElements = document.querySelectorAll('#shirt-select-element option');
-
-//   // 2a) Log both variables to the console to confirm their values
-//   console.log(shirtSelectElement);
-//   console.log(shirtOptionElements);
-//   // 2b) Use the second variable and bracket notation to select one of the option elements and try logging it out to confirm its value 
-//   console.log(shirtOptionElements[1]);
-//   // 2c) Use dot notation to set the `selected` property to true on the `option` element you selected above
-//   shirtOptionElements[1].selected = true;
-//   // 2d) Refresh the browser to see the new value in the `select` element
-//   // 2e) Experiment with setting the `selected` property to true or false on different option elements to see the results
-
-
-// /**
-//  * Event listener for checkboxes
-//  */
-// shirtSelectElement.addEventListener('change', (event) => {
-
-// // 3) Follow the steps below to highlight the selected shirt 
-//   // 3a) Log out the value of the `select` element with `event.target` technique — console.log(event.target.value);
-// //  console.log(event.target.value);
-//   // 3b) Create a variable to store the shirt `img` elements, using a querySelectorAll, like so — ('.shirt-image') 
-//   let shirtImages = document.querySelectorAll('.shirt-image');
-//   // 3c) Create a `for` loop to iterate over the shirt `img` elements in the variable above
-//   for (let i = 0; i < shirtImages.length; i++) {
-//     // 3d) Inside the loop, create the following two variables:
-//     // One to store the alt attribute of the image at the loop's current iteration, like so: YourImgVariableName[i].alt
-//     let imageAlts = shirtImages[i].alt;
-//     // One for the `event.target.value`
-//     let target = event.target.value;
-//     // 3e) Log out the two variables, refresh the page, and select a new shirt to confirm their values
-//     console.log(imageAlts);
-//     console.log(target);
-//     // 3f) Still inside the loop, create an if/else statement 
-//     if(imageAlts === target) {
-//       // 3g) If the two variables you just created are equal, use the `.classList.add()` method to add the 'chosen' className to the img element
-//       shirtImages[i].classList.add('chosen');
-//     } else {
-//       // 3h) Else use the `.classList.remove()` method to remove the 'chosen' className from the from the img element
-//       shirtImages[i].classList.remove('chosen');
-//     } 
-//   }
-//   console.log("The select element's change event listener is functional!");
-// });
+    // Loop over all checkboxes on the page
+    for (let i = 0; i < checkboxes.length; i++) {
+        let checkboxTime = checkboxes[i].getAttribute('data-day-and-time');
+        
+        // If the clicked time and current iteration time match, plus they aren't the same checkbox, then do some stuff
+        if (clickedTime === checkboxTime && clicked !== checkboxes[i]) {
+            // Disable or enable current checkbox
+            if (clicked.checked) {
+                checkboxes[i].disabled = true;
+            } else {
+                checkboxes[i].disabled = false;
+            }
+        }
+    }
+});
